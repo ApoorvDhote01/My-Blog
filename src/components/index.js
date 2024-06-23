50ad76bb078bba2a00f2490c47f88197470ef057
Apoorv <apoorvdhote2001@gmail.com>
2024-06-23 14:22:57 +0530

    Blog Project

---
+++ src/components/index.js
@@ -0,0 +1,31 @@
+import Header from "./Header/header";
+import Logo from "./Logo";
+import RTE from "./RTE";
+import Footer from "./Footer/footer";
+import Container from "./container/Container"
+import LogoutBtn from "./Header/LogoutBtn";
+import SignUp  from "./SignUp";
+import Login from "./Login";
+import Button from "./Button"
+import PostForm from './post-form/PostForm';
+import PostCard from "./PostCard";
+import AuthLayout from "./AuthLayout";
+import Input from "./Input"
+import Select from './Select'
+ 
+export {
+    Header, 
+    Footer,
+    Container,
+    LogoutBtn,
+    Logo,
+    Button,
+    Input,
+    Select,
+    RTE,
+    SignUp,
+    Login,
+    PostForm,
+    PostCard,
+    AuthLayout
+}
